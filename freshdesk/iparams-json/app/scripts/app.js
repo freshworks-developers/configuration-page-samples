3532cbaae30f027ab970b28045bcb017a13a859e
Saif Ali Shaik <saifshine7@gmail.com>
2021-11-15 21:40:02 +0530

    📦 NEW: convert .then to async await

--- freshdesk/iparams-json/app/scripts/app.js
+++ freshdesk/iparams-json/app/scripts/app.js
@@ -1,46 +1,25 @@
-document.onreadystatechange = whenInteractive;
+var client;
 
-function whenInteractive() {
-  if (document.readyState === "interactive") {
-    return app.initialized().then(getClientAPI).catch(console.error);
-  }
-}
+(async function init() {
+  client = await app.initialized();
+  client.events.on('app.activated', makeAPIcall);
+})();
 
-function getClientAPI(_client) {
-  window.client = _client;
-  // For apps those run in background,the callback is invoked everytime page is opened by the Freshdesk Agent.
-  client.events.on("app.activated", makeAPIcall);
-}
+async function getContacts() {
+  const iparamData = await client.iparams.get('creatorDomain');
+  const URL = `https://${iparamData.creatorDomain}.freshdesk.com/api/v2/contacts`;
+  const options = {
+    headers: {
+      Authorization: `Basic <%= encode(iparam.api_key) %>`, // substitution happens by platform
+      'Content-Type': 'application/json'
+    }
+  };
 
-function makeAPIcall() {
-  client.iparams
-    .get("creatorDomain")
-    .then(function (data) {
-      const URL =
-        `https://${data.creatorDomain}.freshdesk.com/api/v2/contacts`;
-      var options = {
-        headers: {
-          Authorization: `Basic <%= encode(iparam.api_key) %>`, // substitution happens by platform
-          "Content-Type": "application/json",
-        },
-      };
+  let { response } = await client.request.get(URL, options);
+  let contacts = JSON.parse(response);
 
-      client.request
-        .get(URL, options)
-        .then(function ({ response }) {
-          let contacts = JSON.parse(response);
-          document.body.insertAdjacentHTML(
-            "beforebegin",
-            "<h2>Listing contacts</h2>"
-          );
-          contacts.forEach(function renderContact({ name }) {
-            return document.body.insertAdjacentHTML(
-              "afterbegin",
-              `${name}<br>`
-            );
-          });
-        })
-        .catch(console.error);
-    })
-    .catch(console.error);
+  document.body.insertAdjacentHTML('beforebegin', '<h2>Listing contacts</h2>');
+  contacts.forEach(function renderContact({ name }) {
+    return document.body.insertAdjacentHTML('afterbegin', `${name}<br>`);
+  });
 }
