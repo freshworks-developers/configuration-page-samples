a145f26551fdf49319427fde1a6b77d53eac18cb
Ajith R <ajithking88@gmail.com>
2022-10-28 11:42:57 +0530

    feat:retrieving iparam values in the app

--- freshteam/iparams-json/app/app.js
+++ freshteam/iparams-json/app/app.js
@@ -1,16 +1,30 @@
-$(document).ready( function() {
-    app.initialized()
-        .then(function(_client) {
-          var client = _client;
-          client.events.on('app.activated',
-            function() {
-                client.data.get('jobPosting')
-                    .then(function(data) {
-                        $('#apptext').text("Job Title: " + data.title);
-                    })
-                    .catch(function(e) {
-                        console.log('Exception - ', e);
-                    });
-        });
-    });
-});
+const getIparams = () => {
+	return client.iparams.get()
+}
+
+const renderApp = async () => {
+	try {
+		const iparams = await getIparams()
+
+		let htmlContent = `<h5>Iparams Values</h5>`
+
+		for (const [key, value] of Object.entries(iparams)) {
+			if (typeof value !== "object") {
+				htmlContent += `
+                <div class="mx-1"><span class="label text-uppercase">${key}</span>
+                : <span class="label">${value}</span><div>`
+			}
+		}
+
+		$("#app").html(htmlContent)
+	} catch (error) {
+		console.log("Error", error)
+		$("#app").html(`Unable to retrieve Iparams Value`)
+	}
+}
+$(document).ready(function () {
+	app.initialized().then(function (_client) {
+		window.client = _client
+		client.events.on("app.activated", renderApp)
+	})
+})
