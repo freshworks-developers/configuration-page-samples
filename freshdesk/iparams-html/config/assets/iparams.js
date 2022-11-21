db8e128c929244d425d6df16c990ffea5d14ed0a
Saif Ali Shaik <saifshine7@gmail.com>
2022-11-21 14:12:19 +0530

    moving js from iparams.html to iparams.js

--- freshdesk/iparams-html/config/assets/iparams.js
+++ freshdesk/iparams-html/config/assets/iparams.js
@@ -1,11 +1,58 @@
 const dropdown = document.querySelector('.select-alien');
 const datepicker = document.querySelector('.datepicker');
+let selectedVal = document.querySelector('.select-alien');
+let apiKey = document.querySelector('.secure-field');
+let selectedDate = document.querySelector('.datepicker');
+let domain = document.querySelector('.domain');
+let nameField = document.querySelector('.text-field');
+
+function postConfigs() {
+    return {
+      __meta: {
+        secure: ['apiKey']
+      },
+      api_key: apiKey.value,
+      transformation: selectedVal.value,
+      domain_url: domain.value,
+      name: nameField.value,
+      date: selectedDate.value
+    };
+  }
+
+function getConfigs(configs) {
+  let { api_key, transformation, domain_url, name, date } = configs;
+  selectedVal.value = transformation;
+  apiKey.value = api_key;
+  selectedDate.value = date;
+  domain.value = domain_url;
+  nameField.value = name;
+  return;
+}
+
+async function validate() {
+  let URL = `https://${domain.value}/api/v2/tickets`;
+  let base64Encoded = btoa(apiKey.value);
+  let options = {
+    headers: {
+      Authorization: `Basic ${base64Encoded}`,
+      'Content-Type': 'application/json'
+    }
+  };
+
+  try {
+    let { status } = await client.request.get(URL, options);
+    if (status == 200) return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
 
 document.onreadystatechange = function () {
   if (document.readyState === 'interactive') renderApp();
   async function renderApp() {
     try {
-      var client = await app.initialized();
+      let client = await app.initialized();
       window.client = client;
     } catch (error) {
       return console.error(error);
